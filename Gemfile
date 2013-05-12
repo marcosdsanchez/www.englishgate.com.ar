91f8a4901165aa2b101c04dedea4a5b8fa463890
Marcos D. Sánchez <marcosdsanchez@gmail.com>
2013-05-12 20:10:51 -0300

    We don't need a complex partial implementation

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'https://rubygems.org'
 ruby  '2.0.0'
 
 gem 'sinatra', '~>1.4.2'
-gem 'sinatra-partial', '~>0.4.0'
 gem 'sinatra-contrib', '~>1.4.0'
 
 gem 'activemodel', '~> 4.0.0.rc1'
--- app/config/views.rb
+++ app/config/views.rb
@@ -1,12 +1,8 @@
-require 'sinatra/partial'
 module EnglishGate
   class Application < Sinatra::Base
-    register Sinatra::Partial
 
-    set :partial_template_engine, :erb
     set :views, root + '/app/views'
     set :public_folder, Proc.new { File.join(root, "public") }
     set :static, true
-    enable :partial_underscores
   end
 end
