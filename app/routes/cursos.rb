603c415d6cce0e76a48832968b87b2697ff2f766
Marcos D. Sánchez <marcosdsanchez@gmail.com>
2013-06-27 12:23:21 -0300

    This page is not used

--- app/routes/cursos.rb
+++
@@ -1,7 +0,0 @@
-module EnglishGate
-  class Application < Sinatra::Base
-    get '/cursos' do
-      erb :cursos
-    end
-  end
-end
--- app/routes/init.rb
+++ app/routes/init.rb
@@ -1,5 +1,4 @@
 require_relative 'before'
 require_relative 'acerca_de'
 require_relative 'contacto'
-require_relative 'cursos'
 require_relative 'ubicacion'
